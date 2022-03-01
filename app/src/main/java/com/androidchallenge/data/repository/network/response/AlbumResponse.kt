274e5e56b91c00f56116cbec202a49eae833479f
Facundo Bayo <facundobayo2@gmail.com>
2022-03-01 03:11:44 -0300

    navigation between fragments using safeargs

--- app/src/main/java/com/androidchallenge/data/repository/network/response/AlbumResponse.kt
+++ app/src/main/java/com/androidchallenge/data/repository/network/response/AlbumResponse.kt
@@ -1,8 +1,12 @@
 package com.androidchallenge.data.repository.network.response
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
 class AlbumResponse(
     val userId: Int = 0,
     val id: Int = 0,
     val title: String = ""
-) {
-}
+) : Parcelable
